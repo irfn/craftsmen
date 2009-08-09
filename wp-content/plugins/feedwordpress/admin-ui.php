faeb08fe3b5f8704847365522db86ca82b64c871
irfn <mail.urfi@gmail.com>
2009-08-09 20:34:53 +0530

    adding feedwordpress

---
+++ wp-content/plugins/feedwordpress/admin-ui.php
@@ -0,0 +1,308 @@
+<?php
+function fwp_linkedit_single_submit ($status = NULL) {
+	if (fwp_test_wp_version(FWP_SCHEMA_25, FWP_SCHEMA_27)) :
+?>
+<div class="submitbox" id="submitlink">
+<div id="previewview"></div>
+<div class="inside"></div>
+
+<p class="submit">
+<input type="submit" name="submit" value="<?php _e('Save') ?>" />
+</p>
+</div>
+<?php
+	endif;
+}
+
+function fwp_linkedit_periodic_submit ($caption = NULL) {
+	if (!fwp_test_wp_version(FWP_SCHEMA_25)) :
+		if (is_null($caption)) : $caption = __('Save Changes &raquo;'); endif;
+?>
+<p class="submit">
+<input type="submit" name="submit" value="<?php print $caption; ?>" />
+</p>
+<?php
+	endif;
+}
+
+function fwp_linkedit_single_submit_closer ($caption = NULL) {
+	if (fwp_test_wp_version(FWP_SCHEMA_27)) :
+		if (is_null($caption)) : $caption = __('Save Changes'); endif;
+?>
+<p class="submit">
+<input class="button-primary" type="submit" name="submit" value="<?php print $caption; ?>" />
+</p>
+<?php
+	endif;
+}
+
+function fwp_authors_single_submit ($link = NULL) {
+	global $wp_db_version;
+	
+	if (fwp_test_wp_version(FWP_SCHEMA_25)) :
+?>
+<div class="submitbox" id="submitlink">
+<div id="previewview">
+</div>
+<div class="inside">
+</div>
+
+<p class="submit">
+<input type="submit" name="save" value="<?php _e('Save') ?>" />
+</p>
+</div>
+<?php
+	endif;
+}
+
+function fwp_option_box_opener ($legend, $id, $class = "stuffbox") {
+	global $wp_db_version;
+	if (isset($wp_db_version) and $wp_db_version >= FWP_SCHEMA_25) :
+?>
+<div id="<?php print $id; ?>" class="<?php print $class; ?>">
+<h3><?php print htmlspecialchars($legend); ?></h3>
+<div class="inside">
+<?php
+	else :
+?>
+<fieldset class="options"><legend><?php print htmlspecialchars($legend); ?></legend>
+<?php
+	endif;
+}
+
+function fwp_option_box_closer () {
+	global $wp_db_version;
+	if (isset($wp_db_version) and $wp_db_version >= FWP_SCHEMA_25) :
+?>
+	</div> <!-- class="inside" -->
+	</div> <!-- class="stuffbox" -->
+<?php
+	else :
+?>
+</fieldset>
+<?php
+	endif;
+}
+
+function fwp_tags_box ($tags, $object) {
+	if (!is_array($tags)) : $tags = array(); endif;
+	
+	$desc = "<p style=\"font-size:smaller;font-style:bold;margin:0\">Tag $object as...</p>";
+
+	if (fwp_test_wp_version(FWP_SCHEMA_28)) : // WordPress 2.8+
+		fwp_option_box_opener(__('Tags'), 'tagsdiv', 'postbox');
+?>
+			<?php print $desc; ?>
+			<div class="tagsdiv" id="post_tag">
+		<div class="jaxtag">
+		 <div class="nojs-tags hide-if-js">
+		  <p><?php _e('Add or remove tags'); ?></p>
+		  <textarea name="tax_input[post_tag]" class="the-tags" id="tax-input[post_tag]"><?php echo implode(",", $tags); ?></textarea>
+		 </div>
+		
+		 <span class="ajaxtag hide-if-no-js">
+			<label class="screen-reader-text" for="new-tag-post_tag"><?php _e('Tags'); ?></label>
+			<input type="text" id="new-tag-post_tag" name="newtag[post_tag]" class="newtag form-input-tip" size="16" autocomplete="off" value="<?php esc_attr_e('Add new tag'); ?>" />
+			<input type="button" class="button tagadd" value="<?php esc_attr_e('Add'); ?>" />
+		 </span>
+		</div>
+		<p class="howto"><?php echo __('Separate tags with commas.'); ?></p>
+		<div class="tagchecklist"></div>
+		</div>
+		<p class="tagcloud-link hide-if-no-js"><a href="#titlediv" class="tagcloud-link" id="link-post_tag"><?php printf( __('Choose from the most used tags in %s'), 'Post Tags'); ?></a></p>
+		</div>
+		</div>
+<?php
+	else :
+		fwp_option_box_opener(__('Tags'), 'tagsdiv', 'postbox');
+?>
+		<?php print $desc; ?>
+		<p id="jaxtag"><input type="text" name="tags_input" class="tags-input" id="tags-input" size="40" tabindex="3" value="<?php echo implode(",", $tags); ?>" /></p>
+		<div id="tagchecklist"></div>
+		</div>
+		</div>
+<?php
+	endif;
+}
+
+function fwp_category_box ($checked, $object, $tags = array()) {
+	global $wp_db_version;
+
+	if (fwp_test_wp_version(FWP_SCHEMA_25)) : // WordPress 2.5.x
+?>
+<div id="category-adder" class="wp-hidden-children">
+    <h4><a id="category-add-toggle" href="#category-add" class="hide-if-no-js" tabindex="3"><?php _e( '+ Add New Category' ); ?></a></h4>
+    <p id="category-add" class="wp-hidden-child">
+	<input type="text" name="newcat" id="newcat" class="form-required form-input-tip" value="<?php _e( 'New category name' ); ?>" tabindex="3" />
+	<?php wp_dropdown_categories( array( 'hide_empty' => 0, 'name' => 'newcat_parent', 'orderby' => 'name', 'hierarchical' => 1, 'show_option_none' => __('Parent category'), 'tab_index' => 3 ) ); ?>
+	<input type="button" id="category-add-sumbit" class="add:categorychecklist:category-add button" value="<?php _e( 'Add' ); ?>" tabindex="3" />
+	<?php wp_nonce_field( 'add-category', '_ajax_nonce', false ); ?>
+	<span id="category-ajax-response"></span>
+    </p>
+</div>
+
+<ul id="category-tabs">
+	<?php /* ui-tabs-selected in WP 2.7 CSS = tabs in WP 2.8 CSS. Thank you, o brilliant wordsmiths of the WordPress 2.8 stylesheet... */ ?>
+	<li class="ui-tabs-selected tabs"><a href="#categories-all" tabindex="3"><?php _e( 'All posts' ); ?></a>
+        <p style="font-size:smaller;font-style:bold;margin:0">Give <?php print $object; ?> these categories</p>
+</li>
+</ul>
+
+<?php /* ui-tabs-panel in WP 2.7 CSS = tabs-panel in WP 2.8 CSS. Thank you, o brilliant wordsmiths of the WordPress 2.8 stylesheet... */ ?>
+<div id="categories-all" class="ui-tabs-panel tabs-panel">
+    <ul id="categorychecklist" class="list:category categorychecklist form-no-clear">
+	<?php fwp_category_checklist(NULL, false, $checked) ?>
+    </ul>
+</div>
+<?php
+	elseif (fwp_test_wp_version(FWP_SCHEMA_20)) : // WordPress 2.x
+?>
+		<div id="moremeta">
+		<div id="grabit" class="dbx-group">
+			<fieldset id="categorydiv" class="dbx-box">
+			<h3 class="dbx-handle"><?php _e('Categories') ?></h3>
+			<div class="dbx-content">
+			<p style="font-size:smaller;font-style:bold;margin:0">Place <?php print $object; ?> under...</p>
+			<p id="jaxcat"></p>
+			<div id="categorychecklist"><?php fwp_category_checklist(NULL, false, $checked); ?></div>
+			</div>
+			</fieldset>
+		</div>
+		</div>
+<?php
+	else : // WordPress 1.5
+?>
+		<fieldset style="width: 60%;">
+		<legend><?php _e('Categories') ?></legend>
+		<p style="font-size:smaller;font-style:bold;margin:0">Place <?php print $object; ?> under...</p>
+		<div style="height: 10em; overflow: scroll;"><?php fwp_category_checklist(NULL, false, $checked); ?></div>
+		</fieldset>
+<?php
+	endif;
+}
+
+function update_feeds_mention ($feed) {
+	echo "<li>Updating <cite>".$feed['link/name']."</cite> from &lt;<a href=\""
+		.$feed['link/uri']."\">".$feed['link/uri']."</a>&gt; ...";
+	flush();
+}
+function update_feeds_finish ($feed, $added, $dt) {
+	echo " completed in $dt second".(($dt==1)?'':'s')."</li>\n";
+}
+
+function fwp_author_list () {
+	global $wpdb;
+	$ret = array();
+
+	// display_name introduced in WP 2.0
+	if (fwp_test_wp_version(FWP_SCHEMA_20)) :
+		$name_column = 'display_name';
+	else :
+		$name_column = 'user_nickname';
+	endif;
+
+	$users = $wpdb->get_results("SELECT * FROM $wpdb->users ORDER BY {$name_column}");
+	if (is_array($users)) :
+		foreach ($users as $user) :
+			$id = (int) $user->ID;
+			$ret[$id] = $user->{$name_column};
+			if (strlen(trim($ret[$id])) == 0) :
+				$ret[$id] = $user->user_login;
+			endif;
+		endforeach;
+	endif;
+	return $ret;
+}
+
+class FeedWordPressSettingsUI {
+	function instead_of_posts_box ($link_id = null) {
+		fwp_option_box_opener('Syndicated Posts, Comments & Pings', 'syndicatedpostsdiv', 'postbox');
+		if (!is_null($link_id)) :
+			$from_this_feed = 'from this feed';
+			$by_default = '';
+			$id_param = "&amp;link_id=".$link_id;
+		else :
+			$from_this_feed = 'from syndicated feeds';
+			$by_default = " by default";
+			$id_param = "";
+		endif;
+?>
+<p>Use the <a href="admin.php?page=<?php print $GLOBALS['fwp_path'] ?>/posts-page.php<?php print $id_param; ?>"><?php _e('Posts'); ?></a>
+settings page to set up how new posts <?php print $from_this_feed; ?> will be published<?php $by_default; ?>, whether they will accept
+comments and pings, any custom fields that should be set on each post, etc.</p>
+<?php
+		fwp_option_box_closer();
+	} /* FeedWordPressSettingsUI::instead_of_posts_box () */
+	
+	function instead_of_authors_box ($link_id = null) {
+		if (!is_null($link_id)) :
+			$from_this_feed = 'from this feed';
+			$by_default = '';
+			$id_param = "&amp;link_id=".$link_id;
+		else :
+			$from_this_feed = 'from syndicated feeds';
+			$by_default = " by default";
+			$id_param = "";
+		endif;
+
+		fwp_option_box_opener('Syndicated Authors', 'authordiv', 'postbox')
+?>
+<p>Use the <a
+href="admin.php?page=<?php print $GLOBALS['fwp_path']
+?>/authors-page.php<?php print $id_param; ?>"><?php _e('Authors');
+?></a> settings page to set up how new posts
+<?php print $from_this_feed; ?> will be assigned to
+authors.</p>
+<?php 
+		fwp_option_box_closer();
+	} /* FeedWordPressSettingsUI::instead_of_authors_box () */
+	
+	function instead_of_categories_box ($link_id = null) {
+		if (!is_null($link_id)) :
+			$from_this_feed = 'from this feed';
+			$by_default = '';
+			$id_param = "&amp;link_id=".$link_id;
+		else :
+			$from_this_feed = 'from syndicated feeds';
+			$by_default = " by default";
+			$id_param = "";
+		endif;
+		
+		fwp_option_box_opener(__('Categories & Tags'), 'categorydiv', 'postbox');
+?>
+<p>Use the <a href="admin.php?page=<?php print $GLOBALS['fwp_path'] ?>/categories-page.php<?php print $id_param; ?>"><?php _e('Categories & Tags'); ?></a>
+settings page to set up how new posts <?php print $from_this_feed; ?> are assigned categories or tags<?php print $by_default; ?>.</p>
+<?php
+		fwp_option_box_closer();
+	} /* FeedWordPressSettingsUI::instead_of_categories_box () */
+
+} /* class FeedWordPressSettingsUI */
+
+function fwp_insert_new_user ($newuser_name) {
+	global $wpdb;
+
+	$ret = null;
+	if (strlen($newuser_name) > 0) :
+		$userdata = array();
+		$userdata['ID'] = NULL;
+		
+		$userdata['user_login'] = sanitize_user($newuser_name);
+		$userdata['user_login'] = apply_filters('pre_user_login', $userdata['user_login']);
+		
+		$userdata['user_nicename'] = sanitize_title($newuser_name);
+		$userdata['user_nicename'] = apply_filters('pre_user_nicename', $userdata['user_nicename']);
+		
+		$userdata['display_name'] = $wpdb->escape($newuser_name);
+
+		$newuser_id = wp_insert_user($userdata);
+		if (is_numeric($newuser_id)) :
+			$ret = $newuser_id;
+		else :
+			// TODO: Add some error detection and reporting
+		endif;
+	else :
+		// TODO: Add some error reporting
+	endif;
+	return $ret;
+} /* fwp_insert_new_user () */
+
---
+++ wp-content/plugins/feedwordpress/syndication-options.php
@@ -0,0 +1,243 @@
+<?php
+require_once(dirname(__FILE__) . '/admin-ui.php');
+
+function fwp_syndication_options_page () {
+        global $wpdb, $wp_db_version, $fwp_path;
+	
+	if (FeedWordPress::needs_upgrade()) :
+		fwp_upgrade_page();
+		return;
+	endif;
+
+	// If this is a POST, validate source and user credentials
+	FeedWordPressCompatibility::validate_http_request(/*action=*/ 'feedwordpress_options', /*capability=*/ 'manage_options');
+
+	if (isset($_POST['create_index'])) :
+		FeedWordPress::create_guid_index();
+?>
+<div class="updated">
+<p><?php _e('Index created on database table.')?></p>
+</div>
+<?php
+	endif;
+
+	if (isset($_POST['submit']) or isset($_POST['create_index'])) :
+		update_option('feedwordpress_cat_id', $_REQUEST['syndication_category']);
+		update_option('feedwordpress_update_logging', $_REQUEST['update_logging']);
+		update_option('feedwordpress_debug', $_POST['feedwordpress_debug']);
+		update_option('feedwordpress_automatic_updates', ($_POST['automatic_updates']=='yes'));
+		update_option('feedwordpress_update_time_limit', ($_POST['update_time_limit']=='yes')?(int) $_POST['time_limit_seconds']:0);
+
+		$freshness_interval = (isset($_POST['freshness_interval']) ? (int) $_POST['freshness_interval'] : 10);
+		update_option('feedworidpress_freshness', $freshness_interval*60);
+
+		if (isset($_REQUEST['hardcode_name']) and ($_REQUEST['hardcode_name'] == 'no')) :
+			update_option('feedwordpress_hardcode_name', 'no');
+		else :
+			update_option('feedwordpress_hardcode_name', 'yes');
+		endif;
+		
+		if (isset($_REQUEST['hardcode_description']) and ($_REQUEST['hardcode_description'] == 'no')) :
+			update_option('feedwordpress_hardcode_description', 'no');
+		else :
+			update_option('feedwordpress_hardcode_description', 'yes');
+		endif;
+
+		if (isset($_REQUEST['hardcode_url']) and ($_REQUEST['hardcode_url'] == 'no')) :
+			update_option('feedwordpress_hardcode_url', 'no');
+		else :
+			update_option('feedwordpress_hardcode_url', 'yes');
+		endif;
+?>
+<div class="updated">
+<p><?php _e('Options saved.')?></p>
+</div>
+<?php
+	endif;
+
+	$cat_id = FeedWordPress::link_category_id();
+	$update_logging = get_option('feedwordpress_update_logging');
+	$feedwordpress_debug = (get_option('feedwordpress_debug')=='yes');
+
+	$update_time_limit = (int) get_option('feedwordpress_update_time_limit');
+	$automatic_updates = get_option('feedwordpress_automatic_updates');
+
+	$freshness_interval = get_option('feedwordpress_freshness');
+	if (false === $freshness_interval) :
+		$freshness_interval = FEEDWORDPRESS_FRESHNESS_INTERVAL;
+	endif;
+	$freshness_interval = $freshness_interval / 60; // convert to minutes
+
+	$hardcode_name = get_option('feedwordpress_hardcode_name');
+	$hardcode_description = get_option('feedwordpress_hardcode_description');
+	$hardcode_url = get_option('feedwordpress_hardcode_url');
+	
+	if (isset($wp_db_version) and $wp_db_version >= 4772) :
+		$results = get_categories(array(
+			"type" => 'link',
+			"hide_empty" => false,	
+		));
+		
+		// Guarantee that the Contributors category will be in the drop-down chooser, even if it is empty.
+		$found_link_category_id = false;
+		foreach ($results as $row) :
+			if ($row->cat_id == $cat_id) :	$found_link_category_id = true;	endif;
+		endforeach;
+		
+		if (!$found_link_category_id) :	$results[] = get_category($cat_id); endif;
+	else :
+		$results = $wpdb->get_results("SELECT cat_id, cat_name, auto_toggle FROM $wpdb->linkcategories ORDER BY cat_id");
+	endif;
+
+	$cats = array_map('trim',
+			preg_split(FEEDWORDPRESS_CAT_SEPARATOR_PATTERN, get_option('feedwordpress_syndication_cats'))
+	);
+	$dogs = SyndicatedPost::category_ids($cats, /*unfamiliar=*/ NULL);
+
+	$tags = array_map('trim',
+			preg_split(FEEDWORDPRESS_CAT_SEPARATOR_PATTERN, get_option('feedwordpress_syndication_tags'))
+	);
+	
+	if (fwp_test_wp_version(FWP_SCHEMA_27)) :
+		$icon = '<div class="icon32"><img src="'.htmlspecialchars(WP_PLUGIN_URL.'/'.$fwp_path.'/feedwordpress.png').'" alt="" /></div>';
+	else :
+		$icon = '';
+	endif;
+
+	if (fwp_test_wp_version(FWP_SCHEMA_26)) :
+		$options = __('Settings');
+	else :
+		$options = __('Options');
+	endif;
+?>
+<script type="text/javascript">
+	function contextual_appearance (item, appear, disappear, value, checkbox) {
+		var rollup=document.getElementById(item);
+		var newuser=document.getElementById(appear);
+		var sitewide=document.getElementById(disappear);
+		if (rollup) {
+			if ((checkbox && rollup.checked) || (!checkbox && value==rollup.value)) {
+				if (newuser) newuser.style.display='block';
+				if (sitewide) sitewide.style.display='none';
+			} else {
+				if (newuser) newuser.style.display='none';
+				if (sitewide) sitewide.style.display='block';
+			}
+		}
+	}
+</script>
+
+<div class="wrap">
+<?php print $icon; ?>
+<h2>Syndication <?php print $options; ?></h2>
+<div id="poststuff">
+<form action="" method="post">
+<?php
+	FeedWordPressCompatibility::stamp_nonce('feedwordpress_options');
+	fwp_linkedit_single_submit();
+?>
+<div id="post-body">
+<?php fwp_option_box_opener('Syndicated Feeds', 'syndicatedfeedsdiv'); ?>
+<table class="editform" width="100%" cellspacing="2" cellpadding="5">
+<tr>
+<th width="33%" scope="row">Syndicated Link category:</th>
+<td width="67%"><?php
+	echo "\n<select name=\"syndication_category\" size=\"1\">";
+	foreach ($results as $row) :
+		if (!isset($row->cat_id)) : $row->cat_id = $row->cat_ID; endif;
+	
+		echo "\n\t<option value=\"$row->cat_id\"";
+		if ($row->cat_id == $cat_id) :
+			echo " selected='selected'";
+		endif;
+		echo ">$row->cat_id: ".wp_specialchars($row->cat_name);
+		if ('Y' == $row->auto_toggle) :
+			echo ' (auto toggle)';
+		endif;
+		echo "</option>\n";
+	endforeach;
+	echo "\n</select>\n";
+?></td>
+</tr>
+
+<tr style="vertical-align: top">
+<th width="33%" scope="row">Check for updates:</th>
+<td width="67%"><select name="automatic_updates" size="1">
+<option value="yes"<?php echo ($automatic_updates)?' selected="selected"':''; ?>>automatically</option>
+<option value="no"<?php echo (!$automatic_updates)?' selected="selected"':''; ?>>only when I request</option>
+</select>
+</td>
+</tr>
+
+<tr style="vertical-align: top">
+<th width="33%" scope="row"><?php print __('Time limit on updates'); ?>:</th>
+<td width="67%"><select id="time-limit" name="update_time_limit" size="1" onchange="contextual_appearance('time-limit', 'time-limit-box', null, 'yes');">
+<option value="no"<?php echo ($update_time_limit>0)?'':' selected="selected"'; ?>>no time limit on updates</option>
+<option value="yes"<?php echo ($update_time_limit>0)?' selected="selected"':''; ?>>limit updates to no more than...</option>
+</select>
+<span id="time-limit-box"><label><input type="text" name="time_limit_seconds" value="<?php print $update_time_limit; ?>" size="5" /> seconds</label></span>
+</tr>
+
+<script type="text/javascript">
+	contextual_appearance('time-limit', 'time-limit-box', null, 'yes');
+</script>
+
+<tr><th width="33%" scope="row" style="vertical-align:top">Feed information:</th>
+<td width="67%"><ul style="margin:0;padding:0;list-style:none">
+<li><input type="checkbox" name="hardcode_name" value="no"<?php echo (($hardcode_name=='yes')?'':' checked="checked"');?>/> Update the contributor title when the feed title changes</li>
+<li><input type="checkbox" name="hardcode_description" value="no"<?php echo (($hardcode_description=='yes')?'':' checked="checked"');?>/> Update when contributor description if the feed tagline changes</li>
+<li><input type="checkbox" name="hardcode_url" value="no"<?php echo (($hardcode_url=='yes')?'':' checked="checked"');?>/> Update the contributor homepage when the feed link changes</li>
+</ul></td></tr>
+</table>
+<?php fwp_linkedit_periodic_submit(); ?>
+<?php fwp_option_box_closer(); ?>
+
+<?php
+	FeedWordPressSettingsUI::instead_of_posts_box();
+	FeedWordPressSettingsUI::instead_of_authors_box();
+	FeedWordPressSettingsUI::instead_of_categories_box();
+	
+	fwp_option_box_opener('Back End', 'backenddiv', 'postbox');
+?>
+<table class="editform" width="100%" cellspacing="2" cellpadding="5">
+<tr style="vertical-align: top">
+<th width="33%" scope="row">Update notices:</th>
+<td width="67%"><select name="update_logging" size="1">
+<option value="yes"<?php echo (($update_logging=='yes')?' selected="selected"':''); ?>>write to PHP logs</option>
+<option value="no"<?php echo (($update_logging!='yes')?' selected="selected"':''); ?>>don't write to PHP logs</option>
+</select></td>
+</tr>
+<tr style="vertical-align: top">
+<th width="33%" scope="row">Debugging mode:</th>
+<td width="67%"><select name="feedwordpress_debug" size="1">
+<option value="yes"<?php echo ($feedwordpress_debug ? ' selected="selected"' : ''); ?>>on</option>
+<option value="no"<?php echo ($feedwordpress_debug ? '' : ' selected="selected"'); ?>>off</option>
+</select>
+<p>When debugging mode is <strong>ON</strong>, FeedWordPress displays many diagnostic error messages,
+warnings, and notices that are ordinarily suppressed, and turns off all caching of feeds. Use with
+caution: this setting is absolutely inappropriate for a production server.</p>
+</td>
+</tr>
+
+<tr style="vertical-align: top">
+<th width="33%" scope="row">Guid index:</th>
+<td width="67%"><input class="button" type="submit" name="create_index" value="Create index on guid column in posts database table" />
+<p>Creating this index may significantly improve performance on some large
+FeedWordPress installations.</p></td>
+</tr>
+</table>
+<?php
+	fwp_option_box_closer();
+	fwp_linkedit_periodic_submit();
+	fwp_linkedit_single_submit_closer();
+?>
+</div>
+</form>
+
+</div> <!-- id="poststuff" -->
+</div> <!-- class="wrap" -->
+<?php
+}
+
+	fwp_syndication_options_page();
+
